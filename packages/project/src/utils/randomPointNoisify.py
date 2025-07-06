68061ec8cac12d7c573e453026fd7cc722cbc9b8
Mihai Bojescu <bojescu.mihai@gmail.com>
2025-07-06 21:44:05 +0300

    chore: Add random range noisify

---
+++ packages/project/src/utils/randomPointNoisify.py
@@ -0,0 +1,36 @@
+import random
+import torch
+from utils.noisify import BaseNoisify
+
+
+class RandomPointNoisify(BaseNoisify):
+    _index_chance: float
+
+    def __init__(
+        self,
+        samples: int,
+        index_chance: float = 0.25,
+        beta_start: float = 0.0025,
+        beta_end: float = 0.05,
+    ):
+        super().__init__(samples, beta_start, beta_end)
+        self._index_chance = index_chance
+
+    def forward(self, x: torch.Tensor) -> list[tuple[torch.Tensor, torch.Tensor]]:
+        picked_indices = [
+            i for i in range(x.shape[-1]) if random.uniform(0, 1) < self._index_chance
+        ]
+        result = [(x, torch.tensor(i)) for i in range(self.samples)]
+
+        for i in range(1, self.samples):
+            previous_x = result[i - 1][0]
+            current_x = result[i][0]
+
+            for j in picked_indices:
+                start = j
+                end = j + 1
+                current_x = self._noisify(previous_x, start, end, i)
+
+            result[i] = (current_x, torch.tensor(i))
+
+        return result
--- packages/project/src/utils/randomRangeNoisify.py
+++ packages/project/src/utils/randomRangeNoisify.py
@@ -3,33 +3,37 @@ import torch
 from utils.noisify import BaseNoisify
 
 
-class RandomNoisify(BaseNoisify):
+class RandomRangeNoisify(BaseNoisify):
     _index_chance: float
 
     def __init__(
         self,
         samples: int,
-        index_chance: float = 0.25,
         beta_start: float = 0.0025,
         beta_end: float = 0.05,
     ):
         super().__init__(samples, beta_start, beta_end)
-        self._index_chance = index_chance
 
     def forward(self, x: torch.Tensor) -> list[tuple[torch.Tensor, torch.Tensor]]:
-        picked_indices = [
-            i for i in range(x.shape[-1]) if random.uniform(0, 1) < self._index_chance
-        ]
+        """
+        The indexes will be generated:
+        - for start: between 0           and 3/4 of length;
+        - for end:   between (start + 1) and 1/1 of length;
+
+        Example: [----------]
+                 ^----------- min start
+                         ^--- max start
+                         ^--- min end
+                            ^ max end
+        """
+        start = random.randint(0, x.shape[-1] - (x.shape[-1] // 4) - 1)
+        end = random.randint(start + 1, x.shape[-1] - 1)
         result = [(x, torch.tensor(i)) for i in range(self.samples)]
 
         for i in range(1, self.samples):
             previous_x = result[i - 1][0]
             current_x = result[i][0]
-
-            for j in picked_indices:
-                start = j
-                end = j + 1
-                current_x = self._noisify(previous_x, start, end, i)
+            current_x = self._noisify(previous_x, start, end, i)
 
             result[i] = (current_x, torch.tensor(i))
 
